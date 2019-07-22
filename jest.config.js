163741a15c17d9acdcf2ccf39d4cf82795036882
Martijn <vandewetering.m@gmail.com>
2019-07-22 21:24:46 +0200

    wip

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,25 @@ module.exports = {
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
   collectCoverage: true,
-  coverageReporters: ['html']
+  transformIgnorePatterns: [],
+  coverageReporters: ['html'],
+  preset: 'jest-preset-angular',
+  setupTestFrameworkScriptFile: "<rootDir>/node_modules/@nrwl/jest/plugins/resolver",
+  collectCoverageFrom: [
+    "apps/**/*.ts",
+    "libs/**/*.ts",
+    "coverage/**/*.ts",
+    "!apps/**/*spec.ts",
+    "!libs/**/*.spec.ts",
+    "!node_modules/**",
+    "!package.json",
+    "!apps/*/src/polyfills.ts",
+    "!apps/*/src/main.ts",
+    "!apps/*/src/test.ts",
+    "!apps/test-app-e2e/**/*"
+  ],
+  moduleNameMapper: {
+    "@monorepo/auth": "<rootDir>/libs/auth/src/index.ts"
+  },
+  testResultsProcessor: 'jest-sonar-reporter'
 };
